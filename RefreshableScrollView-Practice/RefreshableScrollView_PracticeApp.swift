0cfcdbb987be4ba439549a773595d3a8def6005f
Berkay-Disli <berkaydisli.work@gmail.com>
2022-10-03 23:49:04 +0300

    Initial Commit

---
+++ RefreshableScrollView-Practice/RefreshableScrollView_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefreshableScrollView_PracticeApp.swift
+//  RefreshableScrollView-Practice
+//
+//  Created by Berkay Disli on 3.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct RefreshableScrollView_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
